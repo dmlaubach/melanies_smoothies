b1ce970bc862cbf2e4000bebe7aab17c8ebfa7df
Derek Laubach <dmlaubach@gmail.com>
2026-03-12 18:48:03 +0100

    Add conversion from Snowpark to pandas DataFrame
    Convert Snowpark DataFrame to pandas DataFrame for LOC function usage.

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,10 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop 
 #st.dataframe(data=my_dataframe, use_container_width=True)
+# Convert the Snowpark Dataframe to a pandas dataframe so we can use the LOC function
+pd_df=m_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients", my_dataframe
